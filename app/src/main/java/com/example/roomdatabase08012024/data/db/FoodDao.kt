a56f2f34bb94d35f3fd38ae1fc3273d5b83833da
phatdroid94 <phatdroid94@gmail.com>
2024-10-14 18:41:38 +0700

    Setup db

---
+++ app/src/main/java/com/example/roomdatabase08012024/data/db/FoodDao.kt
@@ -0,0 +1,8 @@
+package com.example.roomdatabase08012024.data.db
+
+import androidx.room.Dao
+
+@Dao
+interface FoodDao {
+
+}
--- app/src/main/java/com/example/roomdatabase08012024/data/db/FoodDatabase.kt
+++ app/src/main/java/com/example/roomdatabase08012024/data/db/FoodDatabase.kt
@@ -1,7 +1,26 @@
 package com.example.roomdatabase08012024.data.db
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.roomdatabase08012024.data.db.entity.FoodEntity
 
-abstract class FoodDatabase: RoomDatabase() {
+@Database(entities = [FoodEntity::class], version = 1)
+abstract class FoodDatabase : RoomDatabase() {
+    private var instance: FoodDatabase? = null
+    abstract fun foodDao()
 
+    fun getDb(context: Context): FoodDatabase? {
+        if (instance == null) {
+            return Room.databaseBuilder(
+                context,
+                this::class.java,
+                "food-database"
+            )
+                .build()
+        }
+
+        return instance
+    }
 }
